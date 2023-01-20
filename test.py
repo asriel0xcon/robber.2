c7a72895ac392a9b58c4641bfd5187ba802723b7
joseph villalobos <villalobosjo@laurelbrook.org>
2023-01-20 09:27:58 -0500

    first commit

--- test.py
+++ test.py
@@ -1,4 +1,43 @@
-name=input("ikie ")
-time=input("71 ")
+import time, os
+elephant = ("                        ____",
+"                   .---'-   \\",
+"      .-----------/          \\",
+"     /           (         ^  |   __",
+"&   (             \        O  /  / .'",
+"._/(              '-'  (.   (_.' /",
+"     \                    \     ./",
+"      |    |       |    |/ '._.'",
+"       )   @).____\|  @ |",
+"   .  /    /       (    | ",
+"  \|, '_:::\  . ..  '_:::\ ..\)."
 
-print(name +" is "+ time)
+)
+os.system('cls')
+for j in range(1,15):
+    os.system('cls')
+    for i in elephant:
+        print(" "*j*5 + i)
+    time.sleep(0.5)
+
+os.system('cls')
+print(
+"""
+                                                                               /eeeeeeeeeee\ 
+                                                                 /RRRRRRRRRR\ /eeeeeeeeeeeee\ /RRRRRRRRRR\ 
+                                                                /RRRRRRRRRRRR\|eeeeeeeeeeeee|/RRRRRRRRRRRR\ 
+                                                               /RRRRRRRRRRRRRR +++++++++++++ RRRRRRRRRRRRRR\ 
+                                                              |RRRRRRRRRRRRRR ############### RRRRRRRRRRRRRR| 
+                                                              |RRRRRRRRRRRRR ######### ####### RRRRRRRRRRRRR| 
+                                                               \RRRRRRRRRRR ######### ######### RRRRRRRRRR/ 
+                                                                 |RRRRRRRRR ########## ######## RRRRRRRR| 
+                                                                |RRRRRRRRRR ################### RRRRRRRRR| 
+                                                                             ######     ###### 
+                                                                             #####       ##### 
+                                                                             #nnn#       #nnn#
+
+
+Art by Morfina and Susie Oviatt on asciiart.eu                                                                             
+"""
+)
+
+input("Press any ENTER to exit.")
